80e1d6a6a2ef3698561bd849aa308bb78333d659
Rennan Kelson <rennankelson@hotmail.com>
2023-01-29 15:08:01 -0300

    Primeiro commit

---
+++ src/test/java/com/api/cepsbr/CepsbrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.cepsbr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CepsbrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
