a7bc020c828c4f6ac98c1cda8f5d4c4d2b9957e0
Rennan Kelson <rennankelson@hotmail.com>
2023-02-04 16:20:45 -0300

    Primeiro teste de controller com bd criado

--- src/main/java/com/api/cepsbr/controller/CepController.java
+++ src/main/java/com/api/cepsbr/controller/CepController.java
@@ -2,19 +2,26 @@ package com.api.cepsbr.controller;
 
 import com.api.cepsbr.model.Endereco;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.websocket.server.PathParam;
 
 @RestController
+@RequestMapping("/api/v1/via-ceps")
 public class CepController {
     @GetMapping ("/status")
     public String getStatus (){
         return "UP";
     }
 
-    @GetMapping("/zipcode/{zipcode}")
-    public Endereco getEnderecoZipCode (@PathParam("zipcode") String zipcode){
-        return Endereco.builder().zipcode(zipcode).buid();
+    @GetMapping("cep/{cep}")
+    public Endereco getEnderecoCep (@PathParam("cep") String cep){
+        return Endereco.builder().cep(cep).build();
+
+//        Endereco endereco = new Endereco;
+//        endereco.setCep(cep);
+//
+//        return endereco;
     }
 }
